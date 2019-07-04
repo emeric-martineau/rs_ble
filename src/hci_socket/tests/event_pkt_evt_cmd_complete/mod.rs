d919b691650392126f3c813a4189ab79f9ef71fc
Emeric MARTINEAU <11473190+bubulemaster@users.noreply.github.com>
2019-07-04 19:01:00 +0200

    Add test bind_user_hci_chanel_raw_hci_event_pkt_evt_cmd_complete_le_set_scan_parameters_cmd()

--- src/hci_socket/tests/event_pkt_evt_cmd_complete/mod.rs
+++ src/hci_socket/tests/event_pkt_evt_cmd_complete/mod.rs
@@ -173,6 +173,81 @@ impl HciCallback for TestReadBdAddrCmdHciEvtDisconnCompleteCallback  {
     }
 }
 
+pub struct TestLeSetScanParametersCmdHciEvtDisconnCompleteCallback {
+    pub is_called: Cell<bool>
+}
+
+impl HciCallback for TestLeSetScanParametersCmdHciEvtDisconnCompleteCallback  {
+    fn state_change(&self, state: HciState) -> bool {
+        assert_eq!(HciState::PoweredOn, state);
+
+        // Call first
+        if self.is_called.get() {
+            panic!("le_scan_enable_set() must be call first!");
+        }
+
+        self.is_called.replace(true);
+
+        false
+    }
+
+    fn address_change(&self, _address: String) -> bool {
+        false
+    }
+
+    fn le_conn_complete(&self, _status: u8, _data: Option<BtLeConnectionComplete>) -> bool {
+        false
+    }
+
+    fn le_conn_update_complete(&self, _status: u8, _handle: u16, _interval: f64, _latency: u16, _supervision_timeout: u16) -> bool {
+        false
+    }
+
+    fn rssi_read(&self, _handle: u16, _rssi: i8) -> bool {
+        false
+    }
+
+    fn disconn_complete(&self, _handle: u16, _reason: u8) -> bool {
+        false
+    }
+
+    fn encrypt_change(&self, handle: u16, encrypt: u8) -> bool {
+        false
+    }
+
+    fn acl_data_pkt(&self, _handle: u16, _cid: u16, _data: Vec<u8>) -> bool {
+        false
+    }
+
+    fn read_local_version(&self, _hci_ver: u8, _hci_rev: u16, _lmp_ver: i8, _manufacturer: u16, _lmp_sub_ver: u16) -> bool {
+        false
+    }
+
+    fn le_scan_parameters_set(&self) -> bool {
+        if ! self.is_called.get() {
+            panic!("le_scan_parameters_set() must be call second!");
+        }
+
+        true
+    }
+
+    fn le_scan_enable_set(&self, _state: HciState) -> bool {
+        false
+    }
+
+    fn le_scan_enable_set_cmd(&self, _enable: bool, _filter_duplicates: bool) -> bool {
+        false
+    }
+
+    fn error(&self, _msg: String) -> bool {
+        false
+    }
+
+    fn le_advertising_report(&self, _status: u8, _typ: u8, _address: String, _address_type: BtLeAddressType, _eir: Vec<u8>, _rssi: i8) -> bool {
+        false
+    }
+}
+
 #[test]
 pub fn bind_user_hci_chanel_raw_hci_event_pkt_evt_cmd_complete_read_local_version_cmd_unsupported() {
     let is_socker_hci = true;
@@ -344,6 +419,52 @@ pub fn bind_user_hci_chanel_raw_hci_event_pkt_evt_cmd_complete_read_bd_addr_cmd(
     };
 
     let callback = TestReadBdAddrCmdHciEvtDisconnCompleteCallback {
+        is_called: Cell::new(false),
+    };
+
+    match Hci::new(None, false, &log, &libc) {
+        Ok(mut hci) => match hci.init(Some(&callback)) {
+            Ok(_) => assert_eq!(true, callback.is_called.get()),
+            Err(e) => panic!("Hci init() {:?}", e)
+        },
+        Err(e) =>  panic!("Hci new() {:?}", e)
+    }
+}
+
+#[test]
+pub fn bind_user_hci_chanel_raw_hci_event_pkt_evt_cmd_complete_le_set_scan_parameters_cmd() {
+    let is_socker_hci = true;
+    let is_socker_l2cap = true;
+    let ioctl_hci_dev_info_call_error: HashMap<c_int, bool> = HashMap::new();
+    let my_device_list = init_device_list_request( 1, true);
+    let bind_sockaddr_hci = init_hci_user(0,1);
+    let mut read_data: Vec<u8> = Vec::new();
+
+    read_data.push(HCI_EVENT_PKT);
+    read_data.push(EVT_CMD_COMPLETE);
+    read_data.push(0x00);
+    read_data.push(0x00);
+    // cmd READ_BD_ADDR_CMD
+    read_data.put_u16_le(LE_SET_SCAN_PARAMETERS_CMD);
+    // Status
+    read_data.push(0x00);
+
+    let mut read_data_map = HashMap::new();
+    read_data_map.insert(0, Cursor::new(read_data));
+
+    let libc = TestLibc::new(
+        is_socker_hci,
+        is_socker_l2cap,
+        ioctl_hci_dev_info_call_error,
+        my_device_list,
+        bind_sockaddr_hci,
+        read_data_map);
+
+    let log = ConsoleLogger {
+        debug_level: true
+    };
+
+    let callback = TestLeSetScanParametersCmdHciEvtDisconnCompleteCallback {
         is_called: Cell::new(false),
     };
 
